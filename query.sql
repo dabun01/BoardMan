870dd75d5f95ef77a6681b8f7564c236b7cc9500
David <dave.abundis4@gmail.com>
2024-12-01 23:12:57 -0600

    Add a new query to the query.sql file

--- query.sql
+++ query.sql
@@ -1,5 +1,38 @@
 -- This file will hold all my sql queries
+-- This is all the SQL queries used on the stats.py file
+-- I have made a few changes to the queries to make them more readable
 
+-- Get all teams
+SELECT T_NAME FROM TEAM;
+
+-- Get all players
+SELECT P_NAME FROM PLAYER ORDER BY P_NAME;
+
+-- Search for a player by name
+SELECT * FROM PLAYER WHERE P_NAME = "Victor Wembanyama";
+
+-- Get the stats of the selected team
+select T_NAME, T_SEED, T_WINS, T_LOSSES, 
+(T_WINS / (T_WINS + T_LOSSES)) as WIN_PERCENTAGE 
+from TEAM 
+where T_NAME = "San Antonio Spurs";
+
+-- Get the stats of the player on the selected team
+select P_NUMBER, P_NAME, P_POSITION, P_PTS, P_AST, P_REB, P_FG, T_NAME 
+FROM PLAYER JOIN TEAM ON P_TEAM_ABBR = T_ABBREVIATION 
+WHERE T_NAME = "San Antonio Spurs";
+
+-- Search players by name
+SELECT P_NAME FROM PLAYER WHERE LOWER(P_NAME) LIKE '%steph%';
+
+-- Search team by conference
+SELECT T_NAME FROM TEAM WHERE LOWER(C_ABBREVIATION) LIKE '%s' ORDER BY T_SEED
+
+-- Get the stats of all the team in the selected conference
+select T_NAME, T_SEED, T_WINS, T_LOSSES, (T_WINS / (T_WINS + T_LOSSES)) as WIN_PERCENTAGE 
+from TEAM  where C_ABBREVIATION LIKE '%s' ORDER BY T_SEED
+
+-- Extra queries for testing
 -- Get all players
 SELECT * FROM PLAYER;
 
--- stats.py
+++ stats.py
@@ -18,7 +18,7 @@ def display_teams():
 
 # Function to fetch and display player names in the Listbox
 def display_names():
-    query = "SELECT P_NAME FROM PLAYER"
+    query = "SELECT P_NAME FROM PLAYER ORDER BY P_NAME"
     cursor.execute(query)
     names = [name[0] for name in cursor.fetchall()]  # Fetch all names from the cursor
     for name in names:
@@ -111,6 +111,23 @@ def search_teams():
     search_query = "SELECT T_NAME FROM TEAM WHERE LOWER(T_NAME) LIKE %s"
     cursor.execute(search_query, (f"%{search_term}%",))
     results = cursor.fetchall()
+    # Clear the Listbox
+    teams_listbox.delete(0, END)
+    
+    # Populate the Listbox with search results
+    for (T_NAME,) in results:
+        teams_listbox.insert(END, T_NAME)
+
+#Function where if in the search bar you type east or west it will display the teams in that conference in the left listbox
+# And in the center textbox the standings of the teams in that conference 
+def search_conference():
+    # Get the search term
+    search_term = search_entry.get().strip().lower()
+    
+    # Query to search teams by conference name
+    search_conf_query = "SELECT T_NAME FROM TEAM WHERE LOWER(C_ABBREVIATION) LIKE %s ORDER BY T_SEED"
+    cursor.execute(search_conf_query, (f"%{search_term}%",))
+    results = cursor.fetchall()
     
     # Clear the Listbox
     teams_listbox.delete(0, END)
@@ -118,6 +135,21 @@ def search_teams():
     # Populate the Listbox with search results
     for (T_NAME,) in results:
         teams_listbox.insert(END, T_NAME)
+    
+    # Query to fetch stats for the selected team
+    stats_query = "select T_NAME, T_SEED, T_WINS, T_LOSSES, (T_WINS / (T_WINS + T_LOSSES)) as WIN_PERCENTAGE from TEAM  where C_ABBREVIATION LIKE %s ORDER BY T_SEED"
+    cursor.execute(stats_query, (search_term,))
+    stats = cursor.fetchall()
+
+    # Clear the stats display area
+    stats_text.delete('1.0', END)
+    # Display the conference standings in the text area
+    x = PrettyTable()
+    x.field_names = ["Team Name", "Seed", "Wins", "Losses", "Win%",]
+    for T_NAME, T_SEED, T_WINS, T_LOSSES, WIN_PERCENTAGE in stats:
+        x.add_row([T_NAME, T_SEED, T_WINS, T_LOSSES, WIN_PERCENTAGE])
+    stats_text.insert(END, x)
+    stats_text.insert(END, "-" * 10 + "\n")
 
 
 # Create the main GUI window
@@ -132,12 +164,15 @@ frm.grid()
 ttk.Label(frm, text="Search").grid(column=0, row=0, padx=0, pady=0, sticky='w')
 search_entry = ttk.Entry(frm, width=18)
 search_entry.grid(column=0, row=0, padx=5, pady=5, sticky='e')
-#search for player
+#search for player button
 search_player_button = ttk.Button(frm, text="Search Player", command=search_players)
 search_player_button.grid(column=1, row=0, padx=5, pady=5, sticky='w')
-#search for team
+#search for team button
 search_team_button = ttk.Button(frm, text="Search Team", command=search_teams)
 search_team_button.grid(column=1, row=0, padx=5, pady=5, sticky='e')
+#search for conference button
+search_conference_button = ttk.Button(frm, text="Search Conference", command=search_conference)
+search_conference_button.grid(column=1, row=0, padx=5, pady=5, sticky='n')
 
 # Add a label for the Listbox
 ttk.Label(frm, text="NBA Players:").grid(column=0, row=1, padx=5, pady=5)
@@ -157,9 +192,6 @@ names_listbox.bind('<<ListboxSelect>>', display_stats)
 # Bind the Listbox click event to the display_team_stats function
 teams_listbox.bind('<<ListboxSelect>>', display_team_stats)
 
-# Add a label for the stats display area
-ttk.Label(frm, text="Stats:").grid(column=1, row=1, padx=3, pady=3, sticky='n')
-
 # Add a Text widget to display the stats
 stats_text = Text(frm, height=20, width=95, wrap=WORD)
 stats_text.grid(column=1, row=1, padx=3, pady=3)
@@ -171,7 +203,6 @@ ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3, padx=5,
 display_names()
 #call the function to populate the listbox with team names
 display_teams()
-
 
 # Run the GUI event loop
 root.mainloop()
